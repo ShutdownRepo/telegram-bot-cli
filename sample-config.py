87e0cd7f1a5d759ed504d5c635671fcd3c684e64
Shutdown <40902872+ShutdownRepo@users.noreply.github.com>
2020-07-08 11:09:27 +0200

    Update sample-config.py

--- sample-config.py
+++ sample-config.py
@@ -1,2 +1,4 @@
+# Those are fake "BOT_TOKEN" and "GROUP_ID" values
+# You need to change those values and rename the file to 'config.py'
 BOT_TOKEN = "1462158942:Adjh5DH48sJKO429q5r4V42D5AZ12D"
 GROUP_ID = "-413516497"
